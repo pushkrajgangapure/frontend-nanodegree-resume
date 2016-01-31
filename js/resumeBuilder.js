471cec2968183baff832b65941177a464a69dbb0
pushkrajgangapure <gangapure.pushkraj@gmail.com>
2016-01-31 22:34:08 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var name = "Pushkraj Gangapure";
+ var role= "Software Architect";
+ 
+ var formattedName=HTMLheaderName.replace("%data",name);
+ 
+ var formatterRole=HtmlheaderRole.replace("%data",role);
+ 
+ $("#header").append(formattedName);
+ $("#header").append(formatteRole);
